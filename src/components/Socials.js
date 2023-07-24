f5f09ad465c7cc013ef54a6f25da9a5063e7ca6b
jimanguba <manguba3@myumanitoba.ca>
2023-07-24 01:40:24 -0500

    home and about pages

--- src/components/Socials.js
+++ src/components/Socials.js
@@ -1,7 +1,25 @@
 import React from 'react';
 
+import {
+  ImFacebook,
+  ImInstagram,
+} from 'react-icons/im';
+
 const Socials = () => {
-  return <div>Socials</div>;
+  return <div className='hidden xl:flex ml-24'>
+    <ul className='flex gap-x-4'>
+      <li>
+        <a href='https://www.facebook.com/photographyrbm/' target='_blank'>
+          <ImFacebook />
+        </a>
+      </li>
+      <li>
+        <a href='https://www.instagram.com/rbmbooth/' target='_blank'>
+          <ImInstagram />
+        </a>
+      </li>
+    </ul>
+  </div>;
 };
 
 export default Socials;
--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,7 +1,22 @@
 import React from 'react';
 
+import Socials from '../components/Socials';
 const Portfolio = () => {
-  return <div>Portfolio</div>;
+  return (
+    <section className='section flex justify-center items-center'>
+      <div className='container mx-auto'>
+        {/* text & img wrapper */}
+        <div className='flex flex-col items-center lg:items-start'>
+          {/* text */}
+          <div className='w-full pt-36 pb-14 lg:pt-0 lg:pb-0 lg:w-auto'>
+            <h1 className='h1 text-center lg:text-left'>Page under construction!</h1>
+            <p className='text-[26px] lg:text-[36px] font-primary mb-4 lg:mb-12'> Check out our socials in the meantime!</p>
+            <Socials />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Portfolio;
